ffc2e05596275f0119fbb01e10bd47f1fc530fcb
Duco Dokter <dokter@w20e.com>
2013-08-27 20:11:12 +0200

    release 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
     ]
 
 setup(name='djinn_events',
-      version="1.0.0",
+      version="1.0.1",
       description='Djinn Intranet Events',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
