83ebb68bc95121e41052d3923948e0c24eed4081
Duco Dokter <dokter@w20e.com>
2014-12-05 11:58:25 +0100

    Move to django 1.6.x

--- djinn_events/urls.py
+++ djinn_events/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 from views.eventviewlet import EventViewlet
 from models.event import Event
 from djinn_contenttypes.views.utils import generate_model_urls
